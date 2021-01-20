2e75dcfceeb034a2c74ac43c44cf38c14f81d25b
BartoszStartek <64890040+BartoszStartek@users.noreply.github.com>
2021-01-20 21:16:02 +0100

    updejt
    zobaczymy czy automated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Wiesiek20210120!");
+    response.end("Hello WiesiekGrubasie!");
 });
 
 const port = process.env.PORT || 1337;
