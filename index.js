387e1f780d3091036c28bf065030cc5092cb4a72
BartoszStartek <64890040+BartoszStartek@users.noreply.github.com>
2021-01-20 21:18:18 +0100

    final changes v1
    final change name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello WiesiekGrubasie!");
+    response.end("Hello Mati!");
 });
 
 const port = process.env.PORT || 1337;
